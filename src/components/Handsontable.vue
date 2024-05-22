50dda36ff9aab1359dedce209adc5b6abe91e469
CJC0512 <ehdchs2006@gmail.com>
2024-05-22 12:30:43 +0900

    [fix] vue3의 composition API 형식으로 변경

--- src/components/Handsontable.vue
+++ src/components/Handsontable.vue
@@ -1,27 +1,43 @@
 <template>
   <div ref="hot"></div>
 </template>
-<script>
+
+<script lang="ts">
+import { defineComponent, onMounted, onBeforeUnmount, ref } from 'vue';
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.css';
-export default {
-  name: 'Handsontable',
+
+export default defineComponent({
+  name: 'HandsontableComponent',
   props: {
     settings: {
       type: Object,
       required: true
     }
   },
-  mounted() {
-    this.hot = new Handsontable(this.$refs.hot, this.settings);
-  },
-  beforeUnmount() {
-    if (this.hot) {
-      this.hot.destroy();
-    }
+  setup(props) {
+    const hot = ref<HTMLElement | null>(null);
+    const hotInstance = ref<Handsontable | null>(null);
+
+    onMounted(() => {
+      if (hot.value) {
+        hotInstance.value = new Handsontable(hot.value, props.settings);
+      }
+    });
+
+    onBeforeUnmount(() => {
+      if (hotInstance.value) {
+        hotInstance.value.destroy();
+      }
+    });
+
+    return {
+      hot
+    };
   }
-};
+});
 </script>
+
 <style scoped>
 @import 'handsontable/dist/handsontable.full.css';
 </style>
