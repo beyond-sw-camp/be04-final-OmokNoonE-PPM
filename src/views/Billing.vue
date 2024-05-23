59eaa5b0f2b38d2d661e4317d3a716c7fd0d9a2f
CJC0512 <ehdchs2006@gmail.com>
2024-05-23 18:29:36 +0900

    [feat] 사용하지 않는 파일 삭제 및 수정

--- src/views/Billing.vue
+++ src/views/Billing.vue
@@ -1,18 +1,15 @@
 <template>
 <div>
-<!--  <DataGrid/>-->
   <DataGridTest/>
 </div>
 </template>
 
 <script>
-// import DataGrid from "@/views/DataGrid.vue";
 import DataGridTest from "@/views/DataGridTest.vue";
 
 export default {
   name: "Billing",
   components: {
-    // DataGrid,
     DataGridTest
   },
 };
