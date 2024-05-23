2d11eaf6765ea23d4e14c432f66f1528ef40f4ff
orlzlL <yerincho06@gmail.com>
2024-05-23 09:30:37 +0900

    [feat] Dashboard화면 (Dashboard2.vue)추가

---
+++ src/views/Dashboard2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="grid-container">
+    <PieGraph />
+    <ColumnGraph />
+    <LineGraph />
+    <Gaugegraph/>
+  </div>
+</template>
+
+<script>
+import PieGraph from "@/views/piegraph.vue"
+import ColumnGraph from "@/views/columngraph.vue"
+import LineGraph from "@/views/linegraph.vue";
+import Gaugegraph from "@/views/gaugegraph.vue";
+
+export default {
+  components: {
+    Gaugegraph,
+    PieGraph,
+    ColumnGraph,
+    LineGraph
+  }
+}
+</script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+}
+</style>
