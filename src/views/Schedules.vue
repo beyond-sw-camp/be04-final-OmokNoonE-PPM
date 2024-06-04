e67b00e315add6707e4eaab84ab7c37e9a7af77a
ms1011 <wkdalstjr94@gmail.com>
2024-06-04 12:33:10 +0900

    #57 [UI] 일정 페이지 일정이 없을 때 나오는 화면 변경

--- src/views/Schedules.vue
+++ src/views/Schedules.vue
@@ -1,16 +1,44 @@
 <template>
-<div>
+<div v-if="projectId">
   <ScheduleSheet/>
+</div>
+<div v-else class="no-dashboard">
+  <span>표시할 일정이 없습니다.</span>
 </div>
 </template>
 
 <script>
 import ScheduleSheet from "@/views/ScheduleSheet.vue";
+import store from "@/store";
+import { useToast } from 'vue-toastification';
 
 export default {
   name: "Billing",
   components: {
     ScheduleSheet
   },
+  computed: {
+    projectId() {
+      return store.getters.projectId;
+    }
+  },
+  mounted() {
+    const toast = useToast(); // 토스트 메시지를 사용할 수 있는 toast 객체를 생성합니다.
+    if (!this.projectId) {
+      toast.warning("표시할 일정이 없습니다."); // projectId가 없는 경우 토스트 메시지를 표시합니다.
+    }
+  }
 };
 </script>
+<style>
+.no-dashboard {
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  color: #868e96;
+  width: 90%;
+  height: 80vh;
+}
+</style>
