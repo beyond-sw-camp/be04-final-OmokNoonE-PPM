8bb7c6001c529c8e29eb6e3663413e72ba2c0ea6
ms1011 <wkdalstjr94@gmail.com>
2024-06-05 11:58:40 +0900

    [feat] API 요청 시 에러 예외처리

--- src/axios/axios-instance.js
+++ src/axios/axios-instance.js
@@ -5,6 +5,20 @@ const BASE_URL = 'http://localhost:8888/'
 // 단순 get요청으로 인증값이 필요없는 경우
 const axiosApi = (url, options) => {
     const instance = axios.create({ baseURL: url, withCredentials: true , ...options })
+
+    instance.interceptors.response.use(function (response) {
+        return response;
+    }, function (error) {
+
+        if (error.response && error.response.status === 400) {
+            console.error('잘못된 요청입니다.');
+        }
+        if (error.response && error.response.status === 500) {
+            console.error('서버 오류입니다.');
+        }
+        return Promise.resolve(error);
+    });
+
     return instance;
 }
 
