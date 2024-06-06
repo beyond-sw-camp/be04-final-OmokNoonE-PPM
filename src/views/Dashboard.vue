e60bbbbf60667c484a64c702308de2e6aab24028
ms1011 <wkdalstjr94@gmail.com>
2024-06-06 22:08:45 +0900

    [fix] 알림, 대시보드 갱신 기능
    알림은 시연을 위한 기능

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -16,7 +16,8 @@ import PieGraph from "@/views/components/PieGraph.vue"
 import LineGraph from "@/views/components/LineGraph.vue";
 import GaugeGraph from "@/views/components/GaugeGraph.vue";
 import store from "@/store";
-import { useToast } from 'vue-toastification'; // vue-toastification 라이브러리에서 useToast 함수를 가져옵니다.
+import { useToast } from 'vue-toastification';
+import {defaultInstance} from "@/axios/axios-instance"; // vue-toastification 라이브러리에서 useToast 함수를 가져옵니다.
 
 export default {
   components: {
@@ -30,7 +31,12 @@ export default {
       return store.getters.projectId;
     }
   },
-  mounted() {
+  async mounted() {
+    try {
+      defaultInstance.get(`/graphs/refresh`); // 프로젝트 ID를 이용해 프로젝트 정보를 가져옵니다.
+    } catch (error) {
+      toast.error("데이터를 불러오는 중 오류가 발생했습니다."); // 데이터를 불러오는 중 오류가 발생한 경우 토스트 메시지를 표시합니다.
+    }
     const toast = useToast(); // 토스트 메시지를 사용할 수 있는 toast 객체를 생성합니다.
     if (!this.projectId) {
       toast.warning("표시할 데이터가 없습니다."); // projectId가 없는 경우 토스트 메시지를 표시합니다.
