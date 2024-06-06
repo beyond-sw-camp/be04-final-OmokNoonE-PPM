4978acf7065e9e9c7d9b4992b1654cef41004ffa
orlzlL <80697609+orlzlL@users.noreply.github.com>
2024-06-06 11:54:40 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import router from "./router";
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import MaterialDashboard from "./material-dashboard";
+import { SERVER_URL } from './config';
 
 const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(MaterialDashboard);
 appInstance.mount("#app");
+console.log('Server URL:', SERVER_URL);
