084c2f92618ff5458ddbb2a8eccf21abab75227a
CJC0512 <ehdchs2006@gmail.com>
2024-06-08 23:53:59 +0900

    [fix] 일정 상세보기 시, 요구사항을 불러오지 못하는 에러 해결
    - Calendar.vue의 get 메소드를 setup()안에서 구현

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,12 +1,12 @@
 <script>
-import {defineComponent, watch} from 'vue'
+import {defineComponent, onMounted, watch} from 'vue'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 // import {createEventId, /*INITIAL_EVENTS*/} from "@/event-utils";
 import store from "@/store";
-import { ref } from 'vue';
+import {ref} from 'vue';
 import {defaultInstance} from "@/axios/axios-instance";
 import router from "@/router";
 import MaterialSchedule from "@/components/MaterialSchedule.vue";
@@ -17,9 +17,11 @@ export default defineComponent({
     MaterialSchedule,
     FullCalendar,
   },
-  setup(){
+  setup() {
     const projectId = store.getters.projectId;
     const schedules = ref([]);
+    const requirementList = ref([]);
+    const projectMembers = ref([]);
 
     const modalOpen = ref(false);
     const modalUrl = ref('');
@@ -32,43 +34,100 @@ export default defineComponent({
       modalOpen.value = true;
     };
 
-
-    defaultInstance.get(`schedules/list/${projectId}`)
-    .then(response => {
-      const schedulesData = response.data.result.viewScheduleByProject;
-      schedules.value = schedulesData.map(schedule => {
-        let color;
-        switch (schedule.scheduleStatus) {
-          case '10301':
-            color = '#ffba26';
-            break;
-          case '10302':
-            color = '#24a8ef';
-            break;
-          case '10303':
-            color = '#61cc39';
-            break;
-          default:
-            color = 'gray';
-        }
-        return {
-          id: schedule.scheduleId,
-          title: schedule.scheduleTitle,
-          start: new Date(...schedule.scheduleStartDate).toISOString().split('T')[0],
-          end: new Date(...schedule.scheduleEndDate).toISOString().split('T')[0],
-          backgroundColor: color,
-          textColor: 'black',
-          // 추가적인 사용자 정의 속성
-          scheduleId: schedule.scheduleId,
-        };
-      });
-      console.log(schedules.value);
-    })
-    .catch(error => {
-      console.error(error);
+    onMounted(async () => {
+      await getSchedules();
+      await getProjectRequirements();
+      await getProjectMembers();
     });
 
-    return { schedules, openModal, modalOpen, modalUrl, copyRequirementList, copyProjectMembers };
+    const getSchedules = async () => {
+      await defaultInstance.get(`schedules/list/${projectId}`)
+          .then(response => {
+            const schedulesData = response.data.result.viewScheduleByProject;
+            schedules.value = schedulesData.map(schedule => {
+              let color;
+              switch (schedule.scheduleStatus) {
+                case '10301':
+                  color = '#ffba26';
+                  break;
+                case '10302':
+                  color = '#24a8ef';
+                  break;
+                case '10303':
+                  color = '#61cc39';
+                  break;
+                default:
+                  color = 'gray';
+              }
+              return {
+                id: schedule.scheduleId,
+                title: schedule.scheduleTitle,
+                start: new Date(...schedule.scheduleStartDate).toISOString().split('T')[0],
+                end: new Date(...schedule.scheduleEndDate).toISOString().split('T')[0],
+                backgroundColor: color,
+                textColor: 'black',
+                // 추가적인 사용자 정의 속성
+                scheduleId: schedule.scheduleId,
+              };
+            });
+            console.log(schedules.value);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+    }
+
+    const getProjectRequirements = async () => {
+      // 요구사항 목록 조회 로직 구현
+      try {
+        const response = await defaultInstance.get(`/requirements/list/${projectId}`);
+        requirementList.value = response.data.result.viewRequirementsList.projectRequirementsList;
+        for (let i = 0; i < requirementList.value.length; i++) {
+          const requirementId = requirementList.value[i].requirementsId;
+          const requirementName = requirementList.value[i].requirementsName;
+          const requirementContent = requirementList.value[i].requirementsContent;
+
+          copyRequirementList.value[i] = {
+            requirementId: requirementId,
+            requirementName: requirementName,
+            requirementContent: requirementContent,
+          };
+        }
+
+        console.log('requirementList :', requirementList);
+        console.log('copyRequirementList :', copyRequirementList);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const getProjectMembers = async () => {
+      try {
+        /* 실질적인 API 주소 확인해야함. Controller 메소드 확인 */
+        const response = await defaultInstance.get(`projectMembers/list/${projectId}`);
+        projectMembers.value = response.data.result.viewProjectMembersByProject;
+        for (let i = 0; i < projectMembers.value.length; i++) {
+          const employeeName = projectMembers.value[i].employeeName;
+          const projectMemberEmployeeId = projectMembers.value[i].projectMemberEmployeeId;      // TODO. projectId로 구성원 조회할때 같이 받아오도록 수정 요청
+          const projectMemberId = projectMembers.value[i].projectMemberId;                      // TODO. projectId로 구성원 조회할때 같이 받아오도록 수정 요청
+          const projectMemberRoleName = projectMembers.value[i].projectMemberRoleName;
+
+          copyProjectMembers.value[i] = {
+            name: employeeName,
+            employeeId: projectMemberEmployeeId,
+            projectMemberId: projectMemberId,
+            roleName: projectMemberRoleName,
+            isChecked: false, // 체크박스에 활용될 Value 추가
+          };
+        }
+        console.log('copySchedule.value : ', copyProjectMembers.value);
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return {schedules, openModal, modalOpen, modalUrl, copyRequirementList, copyProjectMembers};
 
   },
   data() {
@@ -104,67 +163,13 @@ export default defineComponent({
     }
   },
   // schedules가 변경될 때마다 calendarOptions의 events를 업데이트합니다.
-  created () {
+  created() {
     watch(() => this.schedules, (newSchedules) => {
       this.calendarOptions.events = newSchedules;
-    }, { immediate: true }); // immediate 옵션을 true로 설정하여 초기에 한 번 실행합니다.
+    }, {immediate: true}); // immediate 옵션을 true로 설정하여 초기에 한 번 실행합니다.
   },
-  async mounted() {
-    await this.getProjectMembers();
-    await this.getRequirementList();
-  },
+
   methods: {
-
-    async getRequirementList() {
-      // 요구사항 목록 조회 로직 구현
-      try {
-        const response = await defaultInstance.get(`/requirements/list/${this.projectId}`);
-        const requirementList = response.data.result.viewRequirementsList.projectRequirementsList;
-        for (let i = 0; i < requirementList.length; i++) {
-          const requirementId = requirementList[i].requirementsId;
-          const requirementName = requirementList[i].requirementsName;
-          const requirementContent = requirementList[i].requirementsContent;
-
-          this.copyRequirementList.value[i] = {
-            requirementId: requirementId,
-            requirementName: requirementName,
-            requirementContent: requirementContent,
-          };
-        }
-
-        console.log('requirementList :', requirementList);
-        console.log('copyRequirementList :', this.copyRequirementList);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-
-    async getProjectMembers () {
-      try {
-        /* 실질적인 API 주소 확인해야함. Controller 메소드 확인 */
-        const response = await defaultInstance.get(`projectMembers/list/${this.projectId}`);
-        const projectMembers = response.data.result.viewProjectMembersByProject;
-        for (let i = 0; i < projectMembers.length; i++) {
-          const employeeName = projectMembers[i].employeeName;
-          const projectMemberEmployeeId = projectMembers[i].projectMemberEmployeeId;      // TODO. projectId로 구성원 조회할때 같이 받아오도록 수정 요청
-          const projectMemberId = projectMembers[i].projectMemberId;                      // TODO. projectId로 구성원 조회할때 같이 받아오도록 수정 요청
-          const projectMemberRoleName = projectMembers[i].projectMemberRoleName;
-
-          this.copyProjectMembers.value[i] = {
-            name: employeeName,
-            employeeId: projectMemberEmployeeId,
-            projectMemberId: projectMemberId,
-            roleName: projectMemberRoleName,
-            isChecked: false, // 체크박스에 활용될 Value 추가
-          };
-        }
-        console.log('copySchedule.value : ', this.copyProjectMembers.value);
-
-      } catch (error) {
-        console.error(error);
-      }
-    },
-
     handleDateSelect(selectInfo) {
       // let title = prompt('Please enter a new title for your event')
       // let calendarApi = selectInfo.view.calendar
