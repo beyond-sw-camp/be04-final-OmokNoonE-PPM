66d3b7353c1bdce16c040ef235a0ad7fcec44d80
ms1011 <wkdalstjr94@gmail.com>
2024-06-09 14:40:43 +0900

    [feat] [UI] 프로젝트 수정내역 보는 기능 구현

--- src/views/AdminProject.vue
+++ src/views/AdminProject.vue
@@ -23,13 +23,6 @@
                   @click="getProjectList">
                 프로젝트 목록
               </material-button>
-              <material-button
-                  :color="projectHistoryButtonColor"
-                  class="register-btn"
-                  style="margin: 0 0 1rem 1rem;"
-                  @click="projectHistoryPageLoad">
-                프로젝트 수정내역
-              </material-button>
               <!-- 프로젝트 목록 테이블 -->
               <table v-if="!projectHistoryButtonColor" class="table align-items-center mb-0">
                 <thead>
@@ -43,32 +36,79 @@
                   <th class="text-uppercase font-weight-bolder text-center">수정일</th>
                   <th class="text-uppercase font-weight-bolder text-center">삭제 여부</th>
                   <th class="text-uppercase font-weight-bolder text-center">삭제일</th>
+                  <th class="text-uppercase font-weight-bolder text-center">수정내역</th>
                 </tr>
                 </thead>
                 <tbody>
-                  <tr v-if="!projectList || projectList.length === 0">
-                    <td colspan="9" class="text-center text-muted" style="height: 320px;">
-                      <span class="no-members-text">등록된 프로젝트가 없습니다.</span>
-                    </td>
-                  </tr>
-                  <tr v-else v-for="project in projectList" :key="project.projectId">
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectId }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectTitle }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectStartDate }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectEndDate }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectStatus }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectCreatedDate }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectModifiedDate }}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectIsDeleted ? 'O' : ''}}</td>
-                    <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectDeletedDate }}</td>
-                  </tr>
+                <tr v-if="!projectList || projectList.length === 0">
+                  <td colspan="9" class="text-center text-muted" style="height: 320px;">
+                    <span class="no-members-text">등록된 프로젝트가 없습니다.</span>
+                  </td>
+                </tr>
+                <tr v-else v-for="project in projectList" :key="project.projectId">
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectId }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectTitle }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectStartDate }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectEndDate }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectStatus }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectCreatedDate }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectModifiedDate }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectIsDeleted ? 'O' : '' }}</td>
+                  <td class="text-center text-xs font-weight-bold mb-0">{{ project.projectDeletedDate }}</td>
+                  <td class="text-center">
+                    <material-button
+                        color="info"
+                        class="register-btn"
+                        style="margin: 0 0 1rem 1rem;"
+                        @click="getProjectHistory(project.projectId)">
+                      수정내역
+                    </material-button>
+                  </td>
+                </tr>
                 </tbody>
               </table>
 
-              <!-- 프로젝트 수정내역 테이블 -->
-              <div v-if="projectHistoryButtonColor" class="card-body centered-content">
+              <!-- 프로젝트 수정내역 modal -->
+              <div class="modal-overlay" @click.self="closeModal" v-if="showProjectHistoryModal">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title">프로젝트 수정내역</h5>
+                      <material-button
+                          color="danger"
+                          class="register-btn"
+                          style="margin: 0 0 1rem 1rem;"
+                          @click="closeModal">
+                        닫기
+                      </material-button>
+                    </div>
 
+                    <div class="modal-body">
+                      <table class="table align-items-center mb-0">
+                        <thead>
+                        <tr>
+                          <th class="text-uppercase font-weight-bolder text-center">사유</th>
+                          <th class="text-uppercase font-weight-bolder text-center">수정일시</th>
+                          <th class="text-uppercase font-weight-bolder text-center">사원번호</th>
+                          <th class="text-uppercase font-weight-bolder text-center">이름</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr v-for="history in projectHistory" :key="history.id">
+                          <td class="text-center text-xs font-weight-bold mb-0">{{ history.projectHistoryReason }}</td>
+                          <td class="text-center text-xs font-weight-bold mb-0">
+                            {{ history.projectHistoryModifiedDate }}
+                          </td>
+                          <td class="text-center text-xs font-weight-bold mb-0">{{ history.employeeId }}</td>
+                          <td class="text-center text-xs font-weight-bold mb-0">{{ history.employeeName }}</td>
+                        </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
               </div>
+
             </div>
           </div>
         </div>
@@ -85,6 +125,9 @@ import {useToast} from 'vue-toastification';
 
 const toast = useToast();
 const projectList = ref([]);
+const projectHistory = ref([]);
+
+const showProjectHistoryModal = ref(false);
 
 const projectListButtonColor = ref("outline-info");
 const projectHistoryButtonColor = ref("");
@@ -100,17 +143,33 @@ async function getProjectList() {
     projectHistoryButtonColor.value = "";
 
     const response = await defaultInstance.get('/projects/admin/list');
+    console.log(response.data)
     projectList.value = response.data.result.viewAllProjectList;
     console.log(projectList.value)
   } catch (error) {
     console.error(error);
-    toast.error('직원 목록을 불러오는데 실패했습니다.');
+    toast.error('프로젝트 목록을 불러오는데 실패했습니다.');
   }
 }
 
-function projectHistoryPageLoad() {
-  projectListButtonColor.value = "";
-  projectHistoryButtonColor.value = "outline-info";
+async function getProjectHistory(projectId) {
+  try {
+    const response = await defaultInstance.get(`/projectHistories/view/${projectId}`);
+    projectHistory.value = response.data.result.viewProjectHistory;
+    if (projectHistory.value.length === 0) {
+      toast.info('수정내역이 없습니다.');
+      return;
+    }
+    showProjectHistoryModal.value = true;
+    console.log(projectHistory.value)
+  } catch (error) {
+    console.error(error);
+    toast.warning('프로젝트 수정내역을 불러오는데 실패했습니다.');
+  }
+}
+
+function closeModal() {
+  showProjectHistoryModal.value = false;
 }
 
 </script>
@@ -148,15 +207,37 @@ function projectHistoryPageLoad() {
   font-size: 1.2rem;
 }
 
-.centered-content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+.modal-overlay {
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100%;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1000;
 }
 
-.contact-inputs {
+.modal-dialog {
+  max-width: 600px;
+  margin: 1.75rem auto;
+}
+
+.modal-content {
+  border-radius: 0.5rem;
+}
+
+.modal-header {
+  border-bottom: none;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1.5rem;
+  border-top-left-radius: 0.5rem;
+  border-top-right-radius: 0.5rem;
 }
+
+.modal-body {
+  padding: 1.5rem;
+}
+
 </style>
